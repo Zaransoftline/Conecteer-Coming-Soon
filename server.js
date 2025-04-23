a0bb9b57e95f6736a4fef52c0f4909a47514b413
Zaransoftline <131307196+Zaransoftline@users.noreply.github.com>
2025-04-23 17:25:20 -0400

    adding firebase

--- server.js
+++ server.js
@@ -3,12 +3,35 @@ const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 const path = require("path");
+const initializeApp = require("firebase/app");
+const getAnalytics = require("firebase/analytics");
 
-const app = express();
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const server = express();
 const PORT = process.env.PORT || 3000;
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./path/to/your/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 // MongoDB schema
 const userSchema = new mongoose.Schema({
@@ -31,8 +54,14 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => console.error('MongoDB error:', err));
 
 
-app.get("/", (req, res) => {
+server.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Sign up route
+
+// server.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
