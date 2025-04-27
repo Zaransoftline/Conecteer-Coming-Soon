f8c381865b8755529163653134dd3a3da80ffc22
Zaransoftline <131307196+Zaransoftline@users.noreply.github.com>
2025-04-26 21:30:37 -0400

    f

--- server.js
+++ server.js
@@ -254,5 +254,8 @@ app.post("/resend-verification", async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
